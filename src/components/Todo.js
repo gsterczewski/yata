639e512e8ab385db87deff0ef22e2cc4be4578a1
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-01-20 16:25:00 +0100

    Todo component styling

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,30 @@
 import React from "react";
 import BasicBar from "./BasicBar";
-export default function Todo({todo, handleToggleCompleted}){
-
+export default function Todo({todo, handleToggle, handleDelete}){
+  const classes = {
+    title : todo.isCompleted ? "todo__title todo__title--completed" : "todo__title",
+    indicator: todo.isCompleted ? "circle-indicator circle-indicator--checked" : "circle-indicator" 
+  }
+  const handleKeyDown = event => {
+    if(event.key === "Enter" || event.key === " " || event.key === "Spacebar"){
+      handleToggle(todo.id)
+    }
+  }
+  const handleClick  = () => {
+    handleToggle(todo.id)
+  }
   return (
     <BasicBar classes="todo">
-    <span className="circle-indicator"></span>
-      <p className="todo__title">{todo.title}</p>
-      <span onClick={handleToggleCompleted}>X</span>
+    <div className={classes.indicator} onClick={handleClick} onKeyDown={handleKeyDown} role="checkbox" aria-checked={todo.isCompleted} aria-label="Toggle todo completed" tabIndex="0">
+      {todo.isCompleted && <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>}
+    </div>
+      <p className={classes.title}>{todo.title}</p>
+      <button onClick={() =>handleDelete(todo.id)}>
+        
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fillRule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
+
+        </button>
+    
     </BasicBar>
     
   )
