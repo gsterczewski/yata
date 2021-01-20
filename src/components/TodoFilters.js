3ecc5a30eeddb92852bbd10fa9eec478b0dd2d17
G.Sterczewski <sterczewski.dev@protonmail.ch>
2021-01-20 17:41:57 +0100

    add separate component for handlig todo filtering

---
+++ src/components/TodoFilters.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import BasicBar from "./BasicBar";
+
+export default function TodoFilters({handleShowAll, handleShowActive, handleShowCompleted}){
+  return (
+    <BasicBar  classes="filters rounded">
+      <button className="filters__button filters__button--active">All</button>
+      <button className="filters__button">Active</button>
+      <button className="filters__button">Completed</button>
+    </BasicBar>
+  )
+}
